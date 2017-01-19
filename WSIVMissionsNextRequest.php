34554676ec9b1799e4b00e6c86266d9dd7a849be
Victor Quach <victorq@theodo.fr>
2017-01-19 11:38:46 +0100

    Use cache

--- WSIVMissionsNextRequest.php
+++ WSIVMissionsNextRequest.php
@@ -1,6 +1,7 @@
 <?php
 class WSIVMissionsNextRequest {
     private $request;
+    private $requestFilename;
     private $location;
     private $uri;
     private $action;
@@ -14,15 +15,17 @@ class WSIVMissionsNextRequest {
     private $return;
     private $perturbations;
     private $missions;
+    private $expire;
 
     public function __construct($idLine, $stationName, $directionSens) {
+        $this->expire   = time() - 5;
         $this->location = "http://opendata-tr.ratp.fr/wsiv/services/Wsiv?wsdl=";
         $this->uri      = "http://opendata-tr.ratp.fr/wsiv/services";
         $this->action   = "urn:getMissionsNext";
         $this->version  = 0;
         $this->client   = new SoapClient(null, array('location' => $this->location,
                                             'uri'       => ""));
-
+        $this->requestFilename = 'cache/' . $idLine . '-' . $stationName . '-' . $directionSens;
         $this->request = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://wsiv.ratp.fr/xsd" xmlns:wsiv="http://wsiv.ratp.fr">
             <soapenv:Header/>
          <soapenv:Body>
@@ -40,19 +43,9 @@ class WSIVMissionsNextRequest {
          </soapenv:Body>
         </soapenv:Envelope>';
 
-        $xmlstring = $this->client->__doRequest($this->request, $this->location, $this->action, $this->version);
-        $clean_xml = str_ireplace(['SOAPENV:', 'NS1:', 'NS2:'], '', $xmlstring);
-        $xml       = simplexml_load_string($clean_xml);
-
-        $this->return        = $xml->Body->getMissionsNextResponse->return;
-        $this->station       = $this->return->argumentStation->name;
-        $this->direction     = $this->return->argumentDirection->name;
-        $this->date          = $this->return->argumentDate;
-        $this->line          = $this->return->argumentLine->reseau->name . ' ' . $this->return->argumentLine->code;
-        $this->perturbations = $this->return->perturbations;
-        $this->missions      = $this->return->missions;
+        $xml = $this->getXML();
+        $this->setAttributesFromXML($xml);
     }
-
 
     public function getStation() {
         return $this->station;
@@ -82,4 +75,26 @@ class WSIVMissionsNextRequest {
         return $this->missions;
     }
 
+    private function getXML() {
+        if(file_exists($this->requestFilename) && filemtime($this->requestFilename) > $this->expire) {
+            $clean_xml = file_get_contents($this->requestFilename);
+            $xml       = simplexml_load_string($clean_xml);
+            return $xml;
+        }
+        $xmlstring = $this->client->__doRequest($this->request, $this->location, $this->action, $this->version);
+        $clean_xml = str_ireplace(['SOAPENV:', 'NS1:', 'NS2:'], '', $xmlstring);
+        file_put_contents($this->requestFilename, $clean_xml);
+        $xml       = simplexml_load_string($clean_xml);
+        return $xml;
+    }
+
+    private function setAttributesFromXML($xml) {
+        $this->return        = $xml->Body->getMissionsNextResponse->return;
+        $this->station       = $this->return->argumentStation->name;
+        $this->direction     = $this->return->argumentDirection->name;
+        $this->date          = $this->return->argumentDate;
+        $this->line          = $this->return->argumentLine->reseau->name . ' ' . $this->return->argumentLine->code;
+        $this->perturbations = $this->return->perturbations;
+        $this->missions      = $this->return->missions;
+    }
 }
